8ad2a079dc5181e7ab1cf568920622c76c5eb08a
Anatoliy <pischanko@gmail.com>
2018-06-11 23:53:09 +0300

    Create 4.php

---
+++ arrays_loops_tasks/4.php
@@ -0,0 +1,9 @@
+<?php
+$arr = ['green'=>'зеленый', 'red'=>'красный','blue'=>'голубой'];
+
+foreach ($arr as $english => $russian)
+    echo $english . '<br>' ;
+
+foreach ($arr as $english => $russian)
+    echo $russian . '<br>' ;
+?>
